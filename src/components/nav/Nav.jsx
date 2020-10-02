604798ede7aa85731bebbfe2ec60bbb2aa818e22
tuanteng96 <tuantengyn@gmail.com>
2020-10-02 22:40:43 +0700

    update

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -6,38 +6,40 @@ import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
 import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
 import EditLocationOutlinedIcon from '@material-ui/icons/EditLocationOutlined';
 import PermIdentityOutlinedIcon from '@material-ui/icons/PermIdentityOutlined';
+import LoyaltyOutlinedIcon from '@material-ui/icons/LoyaltyOutlined';
 
 export default class Nav extends React.Component {
 
     constructor() {
         super();
         this.state = {
-           
+            isActive: true
         }
     }
 
     render() {
-      return(
-          <div className="page-nav">
-              <ul>
-                    {/* <li>
-                        <HomeOutlinedIcon />
-                        <b>Ưu đãi</b>
+        const isProps = this.props;
+        return(
+            <div className="page-nav">
+                <ul className="page-nav__menu page-nav__4">
+                        <li className={isProps.itemActive === "news" ? "active" : ""}>
+                            <LoyaltyOutlinedIcon />
+                            <b>Ưu đãi</b>
+                        </li>
+                        <li>
+                            <ShoppingCartOutlinedIcon />
+                            <b>Sản phẩm</b>
                     </li>
                     <li>
-                        <ShoppingCartOutlinedIcon />
-                        <b>Sản phẩm</b>
-                   </li>
-                   <li>
-                        <EditLocationOutlinedIcon />
-                        <b>Sản phẩm</b>
-                   </li>
-                   <li>
-                       <PermIdentityOutlinedIcon/>
-                       <b>Tài khoản</b>
-                   </li> */}
-              </ul>
-          </div>
-      );
+                            <EditLocationOutlinedIcon />
+                            <b>Liên hệ</b>
+                    </li>
+                    <li>
+                        <PermIdentityOutlinedIcon/>
+                        <b>Tài khoản</b>
+                    </li>
+                </ul>
+            </div>
+        );
     }
 }
--- src/pages/news.jsx
+++ src/pages/news.jsx
@@ -50,27 +50,27 @@ export default class extends React.Component {
             <div className="page-custom ezs-nav ezs-navbar">
                 <NavBar />
                 <div className="page-render">
-                <div class="blocks"><p>Tab 1 content</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam enim quia molestiae facilis laudantium voluptates obcaecati officia cum, sit libero commodi. Ratione illo suscipit temporibus sequi iure ad laboriosam accusamus?</p><p class="">Saepe explicabo voluptas ducimus provident, doloremque quo totam molestias! Suscipit blanditiis eaque exercitationem praesentium reprehenderit, fuga accusamus possimus sed, sint facilis ratione quod, qui dignissimos voluptas! Aliquam rerum consequuntur deleniti.</p><p>Totam reprehenderit amet commodi ipsum nam provident doloremque possimus odio itaque, est animi culpa modi consequatur reiciendis corporis libero laudantium sed eveniet unde delectus a maiores nihil dolores? Natus, perferendis.</p><p>Atque quis totam repellendus omnis alias magnam corrupti, possimus aspernatur perspiciatis quae provident consequatur minima doloremque blanditiis nihil maxime ducimus earum autem. Magni animi blanditiis similique iusto, repellat sed quisquam!</p><p>Suscipit, facere quasi atque totam. Repudiandae facilis at optio atque, rem nam, natus ratione cum enim voluptatem suscipit veniam! Repellat, est debitis. Modi nam mollitia explicabo, unde aliquid impedit! Adipisci!</p><p>Deserunt adipisci tempora asperiores, quo, nisi ex delectus vitae consectetur iste fugiat iusto dolorem autem. Itaque, ipsa voluptas, a assumenda rem, dolorum porro accusantium, officiis veniam nostrum cum cumque impedit.</p><p>Laborum illum ipsa voluptatibus possimus nesciunt ex consequatur rem, natus ad praesentium rerum libero consectetur temporibus cupiditate atque aspernatur, eaque provident eligendi quaerat ea soluta doloremque. Iure fugit, minima facere.</p></div>
-                <div class="blocks"><p>Tab 1 content</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam enim quia molestiae facilis laudantium voluptates obcaecati officia cum, sit libero commodi. Ratione illo suscipit temporibus sequi iure ad laboriosam accusamus?</p><p class="">Saepe explicabo voluptas ducimus provident, doloremque quo totam molestias! Suscipit blanditiis eaque exercitationem praesentium reprehenderit, fuga accusamus possimus sed, sint facilis ratione quod, qui dignissimos voluptas! Aliquam rerum consequuntur deleniti.</p><p>Totam reprehenderit amet commodi ipsum nam provident doloremque possimus odio itaque, est animi culpa modi consequatur reiciendis corporis libero laudantium sed eveniet unde delectus a maiores nihil dolores? Natus, perferendis.</p><p>Atque quis totam repellendus omnis alias magnam corrupti, possimus aspernatur perspiciatis quae provident consequatur minima doloremque blanditiis nihil maxime ducimus earum autem. Magni animi blanditiis similique iusto, repellat sed quisquam!</p><p>Suscipit, facere quasi atque totam. Repudiandae facilis at optio atque, rem nam, natus ratione cum enim voluptatem suscipit veniam! Repellat, est debitis. Modi nam mollitia explicabo, unde aliquid impedit! Adipisci!</p><p>Deserunt adipisci tempora asperiores, quo, nisi ex delectus vitae consectetur iste fugiat iusto dolorem autem. Itaque, ipsa voluptas, a assumenda rem, dolorum porro accusantium, officiis veniam nostrum cum cumque impedit.</p><p>Laborum illum ipsa voluptatibus possimus nesciunt ex consequatur rem, natus ad praesentium rerum libero consectetur temporibus cupiditate atque aspernatur, eaque provident eligendi quaerat ea soluta doloremque. Iure fugit, minima facere.</p></div>
-                    {/* <Swiper
-                        spaceBetween={50}
-                        slidesPerView={1}
-                        >
-                            { arrNews && arrNews.map((item,index) => 
-                                {
-                                    if(index >= 2) return null;
-                                    return (
-                                        <SwiperSlide key={item.ID}>{item.Title}</SwiperSlide>
-                                    )
-                                }
-                            )}
-                        
-                    </Swiper> */}
+                  <div className="blocks"><p>Tab 1 content</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam enim quia molestiae facilis laudantium voluptates obcaecati officia cum, sit libero commodi. Ratione illo suscipit temporibus sequi iure ad laboriosam accusamus?</p><p class="">Saepe explicabo voluptas ducimus provident, doloremque quo totam molestias! Suscipit blanditiis eaque exercitationem praesentium reprehenderit, fuga accusamus possimus sed, sint facilis ratione quod, qui dignissimos voluptas! Aliquam rerum consequuntur deleniti.</p><p>Totam reprehenderit amet commodi ipsum nam provident doloremque possimus odio itaque, est animi culpa modi consequatur reiciendis corporis libero laudantium sed eveniet unde delectus a maiores nihil dolores? Natus, perferendis.</p><p>Atque quis totam repellendus omnis alias magnam corrupti, possimus aspernatur perspiciatis quae provident consequatur minima doloremque blanditiis nihil maxime ducimus earum autem. Magni animi blanditiis similique iusto, repellat sed quisquam!</p><p>Suscipit, facere quasi atque totam. Repudiandae facilis at optio atque, rem nam, natus ratione cum enim voluptatem suscipit veniam! Repellat, est debitis. Modi nam mollitia explicabo, unde aliquid impedit! Adipisci!</p><p>Deserunt adipisci tempora asperiores, quo, nisi ex delectus vitae consectetur iste fugiat iusto dolorem autem. Itaque, ipsa voluptas, a assumenda rem, dolorum porro accusantium, officiis veniam nostrum cum cumque impedit.</p><p>Laborum illum ipsa voluptatibus possimus nesciunt ex consequatur rem, natus ad praesentium rerum libero consectetur temporibus cupiditate atque aspernatur, eaque provident eligendi quaerat ea soluta doloremque. Iure fugit, minima facere.</p></div>
+                      {/* <Swiper
+                          spaceBetween={50}
+                          slidesPerView={1}
+                          >
+                              { arrNews && arrNews.map((item,index) => 
+                                  {
+                                      if(index >= 2) return null;
+                                      return (
+                                          <SwiperSlide key={item.ID}>{item.Title}</SwiperSlide>
+                                      )
+                                  }
+                              )}
+                          
+                      </Swiper> */}
                 </div>
                 <Nav 
                     itemNumber="4"
                     itemIcon="true"
                     itemPosition="bottom"
+                    itemActive="news"
                 />
             </div>
           </Page>

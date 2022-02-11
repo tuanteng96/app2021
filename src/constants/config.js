d9a8df9257ba8cc6a0eb79b942108cde4008cc3b
tuanteng96 <tuantengyn@gmail.com>
2022-02-11 17:33:38 +0700

    update

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,4 +1,3 @@
-export const SERVER_APP = "https://hanababyspa.com";
-export const FINGER_TOKEN = "Kv3a9P7cLwA6t2srocM9";
-export const NAME_APP = "Hana Baby Spa";
+export const SERVER_APP = "https://cser.vn";
+export const NAME_APP = "CSER BEAUTY";
 export const VERSION_APP = "1.0.0";
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,14 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCWHH0ZL8ZVNn9VcKQGQ5gHmEdCvbd4a0E",
-    authDomain: "hanababy-d0a49.firebaseapp.com",
-    databaseURL: "https://hanababy-d0a49-default-rtdb.firebaseio.com",
-    projectId: "hanababy-d0a49",
-    storageBucket: "hanababy-d0a49.appspot.com",
-    messagingSenderId: "600330747110",
-    appId: "1:600330747110:web:7bb10ee6926ca5ff0bcf27",
-    measurementId: "G-PQLSK1E8RR"
+    apiKey: "AIzaSyBOuJ19sg_yuRvrNZ5wo6rW8uGylsnVkws",
+    authDomain: "cser-d0ce7.firebaseapp.com",
+    databaseURL: "https://cser-d0ce7.firebaseio.com",
+    projectId: "cser-d0ce7",
+    storageBucket: "cser-d0ce7.appspot.com",
+    messagingSenderId: "1087460686681",
+    appId: "1:1087460686681:web:5ecbbb8bcd5596faf3aa48"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);

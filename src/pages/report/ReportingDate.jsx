c368bd773dd413ae13b0b0e8b75feb624ebf1707
tuanteng96 <tuantengyn@gmail.com>
2022-05-17 17:29:53 +0700

    update

--- src/pages/report/ReportingDate.jsx
+++ src/pages/report/ReportingDate.jsx
@@ -64,7 +64,108 @@ export default class ReportingDate extends React.Component {
           </div>
         </Navbar>
         <div className="page-render">
-          Báo cáo ngày
+          <div className="bg-white p-15px mb-15px rounded">
+            <div className="text-uppercase text-black fw-600 mb-10px">
+              Thu chi hôm nay
+            </div>
+            <div className="border-bottom-dashed d--f jc--sb ai--c py-8px">
+              <div className="text-gray-700 font-size-xs fw-500">Tổng thu</div>
+              <div className="fw-600 font-size-sm text-success">
+                <i className="las la-arrow-down"></i> 15,000,000
+              </div>
+            </div>
+            <div className="d--f jc--sb ai--c pt-8px">
+              <div className="text-gray-700 font-size-xs fw-500">Tổng chi</div>
+              <div className="fw-600 font-size-sm text-danger">
+                <i className="las la-arrow-up"></i> 8,000,000
+              </div>
+            </div>
+          </div>
+
+          <div className="bg-white p-15px mb-15px rounded">
+            <div className="text-uppercase text-black fw-600 mb-10px">
+              Khách hàng
+            </div>
+            <div className="border-bottom-dashed d--f jc--sb ai--c py-8px">
+              <div className="text-gray-700 font-size-xs fw-500">
+                Khách hàng mới
+              </div>
+              <div className="fw-600 font-size-sm">50</div>
+            </div>
+            <div className="border-bottom-dashed d--f jc--sb ai--c py-8px">
+              <div className="text-gray-700 font-size-xs fw-500">
+                Đến tại Spa
+              </div>
+              <div className="fw-600 font-size-sm">12</div>
+            </div>
+            <div className="border-bottom-dashed d--f jc--sb ai--c py-8px">
+              <div className="text-gray-700 font-size-xs fw-500">Web / App</div>
+              <div className="fw-600 font-size-sm">25</div>
+            </div>
+            <div className="border-bottom-dashed d--f jc--sb ai--c py-8px">
+              <div className="text-gray-700 font-size-xs fw-500">
+                Tổng khách CheckIn
+              </div>
+              <div className="fw-600 font-size-sm">25</div>
+            </div>
+            <div className="d--f jc--sb ai--c pt-8px">
+              <div className="text-gray-700 font-size-xs fw-500">
+                Khách đang CheckIn
+              </div>
+              <div className="fw-600 font-size-sm">16</div>
+            </div>
+          </div>
+
+          <div className="bg-white p-15px mb-15px rounded">
+            <div className="text-uppercase text-black fw-600 mb-10px">
+              Bán hàng
+            </div>
+            <div className="border-bottom-dashed d--f jc--sb ai--c py-8px">
+              <div className="text-gray-700 font-size-xs fw-500">
+                Đơn hàng mới
+              </div>
+              <div className="fw-600 font-size-sm">50</div>
+            </div>
+            <div className="border-bottom-dashed d--f jc--sb ai--c py-8px">
+              <div className="text-gray-700 font-size-xs fw-500">Doanh số</div>
+              <div className="fw-600 font-size-sm">12,000,000</div>
+            </div>
+            <div className="border-bottom-dashed d--f jc--sb ai--c py-8px">
+              <div className="text-gray-700 font-size-xs fw-500">
+                Thanh toán
+              </div>
+              <div className="fw-600 font-size-sm">8,000,000</div>
+            </div>
+            <div className="d--f jc--sb ai--c pt-8px">
+              <div className="text-gray-700 font-size-xs fw-500">
+                Thanh toán nợ
+              </div>
+              <div className="fw-600 font-size-sm">4,000,000</div>
+            </div>
+          </div>
+
+          <div className="bg-white p-15px mb-5px rounded">
+            <div className="text-uppercase text-black fw-600 mb-10px">
+              Dịch vụ
+            </div>
+            <div className="border-bottom-dashed d--f jc--sb ai--c py-8px">
+              <div className="text-gray-700 font-size-xs fw-500">Đặt lịch</div>
+              <div className="fw-600 font-size-sm">50</div>
+            </div>
+            <div className="border-bottom-dashed d--f jc--sb ai--c py-8px">
+              <div className="text-gray-700 font-size-xs fw-500">
+                Dịch vụ đang làm
+              </div>
+              <div className="fw-600 font-size-sm text-warning">15</div>
+            </div>
+            <div className="d--f jc--sb ai--c pt-8px">
+              <div className="text-gray-700 font-size-xs fw-500">
+                Dịch vụ hoàn thành
+              </div>
+              <div className="fw-600 font-size-sm text-success">4</div>
+            </div>
+          </div>
+
           {/* <PageNoData text="Đang cập nhập ..." /> */}
         </div>
         <Filters

a6c33e0845d92e40146d0503da6e9bb2fde4b1bd
tuanteng96 <tuantengyn@gmail.com>
2022-06-27 18:15:30 +0700

    update

--- src/pages/report/IframeReport.jsx
+++ src/pages/report/IframeReport.jsx
@@ -54,7 +54,7 @@ function IframeReport({ f7 }) {
   return (
     <IframeComm
       attributes={{
-        src: `${SERVER_APP}/App23/index.html`,
+        src: `${SERVER_APP}/App23/index.html?v=${new Date().valueOf()}`,
         width: "100%",
         height: "100%",
         frameBorder: 0,
